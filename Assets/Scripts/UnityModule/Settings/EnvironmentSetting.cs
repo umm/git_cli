5a630db954864477890a610028b31c2be93a132d
Tetsuya Mori <monry84@gmail.com>
2017-10-05 23:20:23 +0900

    :sparkles: Implement setting for git path

---
+++ Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityModule.Settings {
+
+    public partial class EnvironmentSetting {
+
+        public partial class EnvironmentSetting_Path {
+
+            private const string DEFAULT_GIT_COMMAND_PATH = "/usr/local/bin/git";
+
+            /// <summary>
+            /// git コマンドのパスの実体
+            /// </summary>
+            [SerializeField]
+            private string commandGit = DEFAULT_GIT_COMMAND_PATH;
+
+            /// <summary>
+            /// git コマンドのパス
+            /// </summary>
+            public string CommandGit {
+                get {
+                    return this.commandGit;
+                }
+                set {
+                    this.commandGit = value;
+                }
+            }
+        }
+
+    }
+
+}
